19c7fb5d7baab488764cf8395a6c0619a1d20cee
c-matsukawa8943 <shoto@hackerz-guild.com>
2025-05-03 01:29:00 +0900

    Firebase設定を環境変数に移行し、認証機能を追加しました。また、.gitignoreに.envファイルを追加しました。

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAaIytxGGH21Jmic771CH5M852afjPbHg0",
-    authDomain: "react-salon-project-2eb00.firebaseapp.com",
-    projectId: "react-salon-project-2eb00",
-    storageBucket: "react-salon-project-2eb00.firebasestorage.app",
-    messagingSenderId: "630766276296",
-    appId: "1:630766276296:web:3e59c15db63feec959dffd"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
